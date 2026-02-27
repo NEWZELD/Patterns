29bc72fbb7b4059d3a3b494f7e5d8868673b3e43
NEWZELD <glebbudaev1@gmail.com>
2026-02-27 19:44:25 +0500

    Добавил комментарии

--- Structural Patterns/Structural Patterns/Structural Patterns.cpp
+++ Structural Patterns/Structural Patterns/Structural Patterns.cpp
@@ -3,65 +3,139 @@
 
 using namespace std;
 
+/// \brief Абстрактный класс напитка
 class Beverage
 {
 public:
+    /**
+    *brief Получение описания напитка
+    * return string - название напитка
+    */
     virtual std::string getDescription() = 0;
+    /**
+    *brief Получение стоимости напитка
+    * return double - цена напитка
+    */
     virtual double cost() = 0;
 };
 
+/// \brief Класс кофе
 class Espresso : public Beverage
 {
 public:
+    /**
+    *brief Функция получения описания кофе
+    */
     string getDescription() override { return "Эспрессо"; }
+    /**
+    *brief Функция получения стоимости кофе
+    */
     double cost() override { return 1.99; }
 };
 
+/// \brief Класс чай
 class Tea : public Beverage
 {
 public:
+    /**
+    *brief Функция получения описания чая
+    */
     string getDescription() override { return "Чай"; }
+    /**
+    *brief Функция получения стоимости чая
+    */
     double cost() override { return 1.50; }
 };
+
+/// \brief Класс декоратора
 class CondimentDecorator : public Beverage
 {
 protected:
     Beverage* beverage;
 public:
+    /**
+    *brief Конструктор декоратора
+    */
     CondimentDecorator(Beverage* b) : beverage(b){}
+    /**
+    *brief Получение описания
+    */
     string getDescription() override { return beverage->getDescription(); }
+    /**
+    *brief Получение стоимости
+    */
     double cost() override { return beverage->cost(); }
 };
 
+/// \brief Декоратор Молока
 class Milk : public CondimentDecorator
 {
 public:
+    /**
+    *brief Конструктор молока
+    */
     Milk(Beverage* b) : CondimentDecorator(b) {}
+    /**
+    *brief Добавление молока к описанию
+    */
     string getDescription() override { return beverage->getDescription() + ", Молоко"; }
+    /**
+    *brief Добавление стоимости молока
+    */
     double cost() override { return beverage->cost() + 0.10; }
 };
 
+/// \brief Декоратор Сахара
 class Sugar : public CondimentDecorator
 {
 public:
+    /**
+    *brief Конструктор сахара
+    */
     Sugar(Beverage* b) : CondimentDecorator(b) {}
+    /**
+    *brief Добавление сахара к описанию
+    */
     string getDescription() override { return beverage->getDescription() + ", Сахар"; }
+    /**
+    *brief Добавление стоимости сахара
+    */
     double cost() override { return beverage->cost() + 0.05; }
 };
 
+/// \brief Декоратор Сиропа
 class Syrup : public CondimentDecorator
 {
 public:
+    /**
+    *brief Конструктор сиропа
+    */
     Syrup(Beverage* b) : CondimentDecorator(b) {}
+    /**
+    *brief Добавление сиропа к описанию
+    */
     string getDescription() override { return beverage->getDescription() + ", Сироп"; }
+    /**
+    *brief Добавление стоимости сиропа
+    */
     double cost() override { return beverage->cost() + 0.15; }
 };
 
+/// \brief Декоратор Шоколада
 class Chocolate : public CondimentDecorator
 {
 public:
+    /**
+    *brief Конструктор шоколада
+    */
     Chocolate(Beverage* b) : CondimentDecorator(b) {}
+    /**
+    *brief Добавление шоколада к описанию
+    */
     string getDescription() override { return beverage->getDescription() + ", Шоколад"; }
+    /**
+    *brief Добавление стоимости шоколада
+    */
     double cost() override { return beverage->cost() + 0.20; }
 };
 int main()
